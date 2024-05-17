e6d4dc35bb8e7ee69b0c40153230ef1736c1b98c
VickArmand <victormaina1962@gmail.com>
2024-05-17 14:42:33 +0300

    annotate function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -74,7 +74,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """ returns a connector to the database"""
     user = os.getenv('PERSONAL_DATA_DB_USERNAME') | "root"
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD') | ""

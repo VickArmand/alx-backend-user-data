0fce5fce20a8c19e8d005b30148a4c02b4ff97b5
VickArmand <victormaina1962@gmail.com>
2024-05-27 01:08:35 +0300

    corrections

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """session_auth module"""
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -25,4 +26,5 @@ class SessionAuth(Auth):
     def current_user(self, request=None):
         """returns a User instance based on a cookie value"""
         session_id = self.session_cookie(request)
-        return self.user_id_for_session_id(session_id)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)

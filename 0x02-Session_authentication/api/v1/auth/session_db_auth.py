0d913b03caf84d9d30c480a617e8fc7a344bf910
VickArmand <victormaina1962@gmail.com>
2024-05-27 06:08:26 +0300

    sessions

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" session_db_auth module
+"""
+
+
+class SessionDBAuth(SessionExpAuth):
+    """"""
+    pass
---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" user_session module
+"""
+
+
+class UserSession(Base):
+    """"""
+    pass

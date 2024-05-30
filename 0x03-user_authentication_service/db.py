5e5219d0add7747cc314b2501976607c28a0e831
VickArmand <victormaina1962@gmail.com>
2024-05-30 12:26:06 +0300

    2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,8 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
+from typing import Tuple
 
 
 class DB:
@@ -36,3 +39,13 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> Tuple:
+        """"""
+        try:
+            record = self._session.query(User).filter_by(**kwargs).one()
+            return record
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise

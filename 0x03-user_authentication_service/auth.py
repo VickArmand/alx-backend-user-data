9e27114a5e90b40a2240261ae040742f36962056
VickArmand <victormaina1962@gmail.com>
2024-06-01 09:23:50 +0300

    pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,11 +104,12 @@ class Auth:
         take an email string argument and returns a string.
         Find the user corresponding to the email.
         If the user does not exist, raise a ValueError exception.
-        If it exists, generate a UUID and update the user’s reset_token database field.
+        If it exists, generate a UUID
+        and update the user’s reset_token database field.
         Return the token.
         """
         user = self._db.find_user_by(email=email)
-        if user:
+        if user is not None:
             token = _generate_uuid()
             self._db.update_user(user.id, reset_token=token)
             return token

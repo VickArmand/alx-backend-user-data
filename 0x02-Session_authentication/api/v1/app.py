e521547925d5a065b93feb7f9baf83ac392574ed
VickArmand <victormaina1962@gmail.com>
2024-06-04 04:07:44 +0300

    corrections

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,12 +42,12 @@ def before_request():
                         '/api/v1/auth_session/login/'
                         ]
         if auth.require_auth(request.path, exclude_list):
-            if auth.current_user(request) is None:
-                abort(403)
             if auth.authorization_header(
                 request) is None and auth.session_cookie(
                     request) is None:
                 abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
     request.current_user = auth.current_user(request)
 
 
